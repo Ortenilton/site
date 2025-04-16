7f3a5ca2d5eec5302c05360e0c64a9f28a081e96
Ortenilton <orteniltonfilho@gmail.com>
2025-04-16 12:13:50 +0100

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para o menu mobile
 function animarMenu() {
     let menuMob = document.querySelector('.menu-mobile')
     if (menuMob.classList.contains('open')) {
@@ -6,10 +7,27 @@ function animarMenu() {
         menuMob.classList.add('open');
     }
 }
-    
+
+// Adiciona evento de click aos links do menu mobile para fechar o menu após clicar
+document.addEventListener('DOMContentLoaded', function() {
+    // Configura o funcionamento do menu mobile
     let linksMenu = document.querySelectorAll('.menu-mobile a');
-        linksMenu.forEach(link => {
+    linksMenu.forEach(link => {
         link.addEventListener('click', () => {
-        animarMenu();
+            animarMenu();
+        });
+    });
+    
+    // Configura as bolhas
+    const bubbles = document.querySelectorAll('.bolhas span');
+    
+    bubbles.forEach(bubble => {
+        // Posição horizontal aleatória
+        const randomLeft = Math.floor(Math.random() * 100);
+        bubble.style.left = `${randomLeft}%`;
+        
+        // Atraso inicial aleatório para criar efeito mais natural
+        const randomDelay = Math.random() * 20;
+        bubble.style.animationDelay = `${randomDelay}s`;
     });
 });
