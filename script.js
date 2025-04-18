72038622e94f0c393c878e916beed98438902c4a
ortenilton <orteniltonfilho@gmail.com>
2025-04-18 14:45:44 +0100

    bolhas ajeitadas

--- script.js
+++ script.js
@@ -34,14 +34,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const randomSize = Math.random() * 20 + 10;
         const randomDuration = Math.random() * 40 + 30;
         const randomDelay = Math.random() * 10;
-        const randomBottom = Math.random() * 20;
         
         bolha.style.setProperty('--i', randomDuration);
         bolha.style.left = `${randomLeft}%`;
         bolha.style.width = `${randomSize}px`;
         bolha.style.height = `${randomSize}px`;
         bolha.style.animationDelay = `${randomDelay}s`;
-        bolha.style.bottom = `${randomBottom}px`;
         
         bolhasContainer.appendChild(bolha);
     }
